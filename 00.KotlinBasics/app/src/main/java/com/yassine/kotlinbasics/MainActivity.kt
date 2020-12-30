67fae2245ba268e9f924bb7c780732dc6338bf18
Yassine Marroun <el.yasin@hotmail.com>
2020-12-30 11:59:49 +0100

    Naming of variables and constants

--- 00.KotlinBasics/app/src/main/java/com/yassine/kotlinbasics/MainActivity.kt
+++ 00.KotlinBasics/app/src/main/java/com/yassine/kotlinbasics/MainActivity.kt
@@ -7,5 +7,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        variablesAndConstants();
+    }
+
+    /*
+    Here we talk about variables and constants
+     */
+    private fun variablesAndConstants() {
+
+        //Variables
+        var myFirstVariable = "Hello"
+        var myFirstNumber = 1
+
+        println(myFirstVariable)
+
+        myFirstVariable = "Changing the value"
+        println(myFirstVariable)
+
+        var mySecondVariable = "This is second"
+        println(mySecondVariable)
+
+        mySecondVariable = myFirstVariable
+        println(mySecondVariable)
+
+        myFirstVariable = "Done"
+        println(myFirstVariable)
+
+        //Constants
+        //A constant cannot change its value
+        val myFirstConstant = "Fixed value"
+        println(myFirstConstant)
     }
 }
