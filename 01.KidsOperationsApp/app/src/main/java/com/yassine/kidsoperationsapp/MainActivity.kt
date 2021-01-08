71f5e7932093a61b2247194ec4997a29a834fef4
Yassine Marroun <el.yasin@hotmail.com>
2021-01-08 15:53:21 +0100

    Added a second Activity

--- 01.KidsOperationsApp/app/src/main/java/com/yassine/kidsoperationsapp/MainActivity.kt
+++ 01.KidsOperationsApp/app/src/main/java/com/yassine/kidsoperationsapp/MainActivity.kt
@@ -1,22 +1,24 @@
 package com.yassine.kidsoperationsapp
 
+import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-
+    private var ivCharacter = findViewById<ImageView>(R.id.imageView_Character)
+    private var etName = findViewById<EditText>(R.id.txt_name)
+    private var tvBestScore = findViewById<TextView>(R.id.textView_BestScore)
+    private var mp = MediaPlayer.create(this, R.raw.alphabet_song)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var ivCharacter = findViewById<ImageView>(R.id.imageView_Character)
-        var etName = findViewById<EditText>(R.id.txt_name)
-        var tvBestScore = findViewById<TextView>(R.id.textView_BestScore)
 
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setIcon(R.mipmap.ic_launcher)
@@ -37,8 +39,26 @@ class MainActivity : AppCompatActivity() {
             id = resources.getIdentifier("sandia", "drawable", packageName)
             ivCharacter.setImageResource(id)
         } else if (randomNumber == 4.toDouble() || randomNumber == 5.toDouble() || randomNumber == 6.toDouble()) {
-            id = resources.getIdentifier("mango", "drawable", packageName)
+            id = resources.getIdentifier("uva", "drawable", packageName)
             ivCharacter.setImageResource(id)
+        }
+
+        mp.start()
+        mp.isLooping = true
+    }
+
+    public fun play(view: View) {
+
+        var name = etName.text.toString()
+
+        if (name != "") {
+            mp.stop()
+            mp.release()
+
+            val intent = Intent(this, MainActivity2_Level1::class.java)
+            //intent.putExtra("player", etName)
+            startActivity(intent)
+            finish()
         }
     }
 }
---
+++ 01.KidsOperationsApp/app/src/main/java/com/yassine/kidsoperationsapp/MainActivity2_Level1.kt
@@ -0,0 +1,11 @@
+package com.yassine.kidsoperationsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2_Level1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity2_level1)
+    }
+}
